b40e72d63fdf6171e3d5745b3e954fb79908e0c8
Neo Mosebi <neosammosebi01@gmail.com>
2025-06-13 22:08:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("task-input");
+    const addTaskBtn = document.getElementById("add-task-btn");
+    const taskList = document.getElementById("task-list");
 
+    addTaskBtn.addEventListener("click", function () {
+        const taskText = taskInput.ariaValueMax.trim();
+
+        if(taskText  !== "") {
+            const li = document.createElement("li");
+            li.textContent = taskText;
+            taskList.appendChild(li);
+            taskInput.value = ""
+        }
+    })
+})

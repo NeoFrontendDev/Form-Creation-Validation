553aac8d1b9123bd1f6db7ff490ea30f9acebe36
Neo Mosebi <neosammosebi01@gmail.com>
2025-06-14 01:58:08 +0200

    Update fetch-data.js

--- fetch-data.js
+++ fetch-data.js
@@ -1,1 +1,21 @@
+async function fetchUserData() {
+  const apiUrl = "https://jsonplaceholder.typicode.com/users";
+  const dataContainer = document.getElementById("api-data");
+
+  try {
+    const respone = await fetch(apiUrl);
+
+    if (!response.ok) {
+    throw new Error ("HTTP error! Status: ${response.status}");
+    }
+    const users = await response.json();
+      console.log("Fetched users:", users);
+    if (users.length > 0) {
+      dataContainer.textContent = "First user: ${users[0].name}";
+    }
+  } catch (error) {
+    console.error("Error fetching data:",error);
+    dataContainer.textContent = "Failed to load data.";
+  }
+}
 
